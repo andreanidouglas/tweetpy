653c836a1b6f2552ff0f06a535103cc5d879670c
Douglas R Andreani <andreani.dr@gmail.com>
2016-12-20 18:23:40 +0000

    more lint corrections

--- tweet.py
+++ tweet.py
@@ -1,3 +1,4 @@
+
 """
 Main twitter module
 """
@@ -8,11 +9,9 @@ from tweetpy import OAuthClient
 from configparser import SafeConfigParser, NoSectionError, NoOptionError, ConfigParser
 
 
-config_file = 'tweet.ini'
-consumer_key=''
-consumer_secret=''
 
-def read_config():
+
+def read_config(config_file):
     """
         read configuration file for twitter credentials
     """
@@ -29,9 +28,18 @@ def read_config():
         return result
     return result
 
-if __name__ == '__main__':
-    TWITTER = OAuthClient(consumer_key, consumer_secret)
-    config = read_config()
+def main():
+    """
+        Gets argv[0] and post it as tweet for the authenticated user
+        if no user available, go through the loggin proccess
+    """
+
+    config_file = 'tweet.ini'
+    consumer_key = 'RzIGV2ovHzmopdFzZmiuH9SXU'
+    consumer_secret = '1ewAqN3il9FjnSKKqoWXhdTop5kycYnDNXQtX7EtMHqZgi2LBc'
+
+    OAuthClient(consumer_key, consumer_secret)
+    config = read_config(config_file)
     if isinstance(config, tuple):
         oauth_secret = config[0]
         oauth_token_secret = config[1]
@@ -39,7 +47,8 @@ if __name__ == '__main__':
         print('Tokens not found')
         client = OAuthClient(consumer_key, consumer_secret)
         tokens = client.fetch_request_token()
-        client.request_user_pin(tokens[b'oauth_token'].decode('utf-8'), tokens[b'oauth_token_secret'].decode('utf-8'))
+        client.request_user_pin(tokens[b'oauth_token'].decode('utf-8'),
+                                tokens[b'oauth_token_secret'].decode('utf-8'))
         response = client.fetch_access_token()
         oauth_secret = response[b'oauth_token'].decode('utf-8')
         oauth_token_secret = response[b'oauth_token_secret'].decode('utf-8')
@@ -48,12 +57,18 @@ if __name__ == '__main__':
                                   'access_token_secret' : oauth_token_secret}
         with open(config_file, 'w') as config_file_desc:
             config.write(config_file_desc)
-            
+
     client = OAuthClient(consumer_key, consumer_secret)
     response = client.post_status(sys.argv[1], None, oauth_secret, oauth_token_secret)
     for resp in response:
         json_response = json.loads(resp.decode('utf-8'))
         formatted_response = "User: {} - said: {} - id: {}"
+        print("")
         print(formatted_response.format(json_response['user']['screen_name'],
                                         json_response['text'],
                                         json_response['id_str']))
+
+
+
+if __name__ == "__main__":
+    main()
